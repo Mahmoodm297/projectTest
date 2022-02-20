d73c48b1bfdc213f0a15976e9c15ba376e7e61d1
mahmoodm297 <mahmoodm297@gmail.com>
2022-02-20 11:25:19 +0200

    add python project to pipeline

--- start-postgres.sh
+++ start-postgres.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ ! -n "`docker ps|grep -w postgress`" ] ; then
-	docker run   -p 5432:5432 --detach -t postgress --name localPostgres 
+	#docker run   -p 5432:5432 --detach -t postgress --name localPostgres 
+	docker run   -p 5432:5432 --detach -t postgress 
 else
 	echo "Postgres already running"
 fi
