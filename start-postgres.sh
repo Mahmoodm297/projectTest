b9526e2371daf413d2aa9feed637df4a3716fea1
mahmoodm297 <mahmoodm297@gmail.com>
2022-02-20 18:31:41 +0200

    updating start postgres to use build name varaiable

--- start-postgres.sh
+++ start-postgres.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! -n "`docker ps|grep -w postgress`" ] ; then
+if [ ! -n "`docker ps|grep -w ${postgress}`" ] ; then
 	#docker run   -p 5432:5432 --detach -t postgress --name localPostgres 
-	docker run   -p 5432:5432 --detach -t postgress 
+	docker run   -p 5432:5432 --detach -t  ${BUILD}
 else
 	echo "Postgres already running"
 fi
