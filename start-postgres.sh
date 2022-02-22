02bb0a02212a46ec6d101d481374bbab3c09b5d5
mahmoodm297 <mahmoodm297@gmail.com>
2022-02-22 13:00:36 +0200

    updating postgres startup script

--- start-postgres.sh
+++ start-postgres.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-echo "Creating Postgres vuild $BUILD"
+#creating volume
+Volume='postgresVol'
+if [ ! -n "`docker volume ls |grep -w ${Volume}`" ]; then
+	docker volume create ${Volume}	
+fi
+echo "Running Postgres Instance $BUILD"
 if [ ! -n "`docker ps|grep -w ${BUILD}`" ] ; then
 	#docker run   -p 5432:5432 --detach -t postgress --name localPostgres 
-	docker run   -p 5432:5432 --detach -t  ${BUILD}
+	docker run  -v ${postgresVol}:${PGDATA } -p 5432:5432 --detach -t  ${BUILD}
 else
 	echo "Postgres already running"
 fi
